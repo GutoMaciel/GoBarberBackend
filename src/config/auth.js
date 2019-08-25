288f8d86f7264edc74d90a2e4231c689e23d97cc
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-08-25 09:30:30 -0300

    back end go barber

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '258e0e48b33588876b49daad903a9547',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'GutoMaciel',
+    email: 'labellidiaeventos@gmail.com',
+    password_hash: '1238712387',
+  });
+
+  return res.json(user);
+});
+
+export default routes;
